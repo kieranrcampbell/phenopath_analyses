7ca2bb773b93bb5c62f740bf067d823bee53b6ad
Kieran Campbell <kieranrcampbell@gmail.com>
2017-03-08 11:57:16 +0000

    plotting for coad

--- scripts/run_cavi.R
+++ scripts/run_cavi.R
@@ -9,10 +9,18 @@ args <- commandArgs(trailingOnly = TRUE)
 input_rds <- args[1]
 output_file <- args[2]
 
+cov_name <- NULL
+
+if(length(args) > 2) {
+  cov_name <- args[3]
+} else {
+  cov_name <- 'x'
+}
+
 sce <- readRDS(input_rds)
 
 y <- scale(t(exprs(sce)))
-x <- cbind(sce$x)
+x <- cbind(pData(sce)[[ cov_name ]])
 
 pcavi <- clvm(y, x)
 
