787fd83dd96f58ef1e5efd2a6859b0fd9d2db4ab
Kieran Campbell <kieranrcampbell@gmail.com>
2017-03-10 12:51:49 +0000

    shalek analysis now working

--- scripts/make_figures.R
+++ scripts/make_figures.R
@@ -27,13 +27,14 @@ df_noise <- gather(df_discrete_noise, covariate, y, -pst)
 
 
 ggplot(df_pred, aes(x = pst, y = y, color = covariate)) +
-  geom_point(data = df_noise, alpha = 0.4) +
+  #geom_point(data = df_noise, alpha = 0.4) +
   geom_line(size = 1.5) +
   scale_color_brewer(name = "Discrete\nCovariate", palette = "Set1") +
-  xlab("Patient trajectory") + ylab("Dynamic observable") +
-  theme(legend.text = element_blank(), legend.direction = "horizontal",
-        legend.position = "top", axis.text = element_blank(),
-        axis.ticks = element_blank(), axis.title = element_text(size = 11))
+  xlab("Trajectory") + ylab("Gene expression") +
+  theme(legend.text = element_blank(), legend.direction = "vertical",
+        legend.position = "left", axis.text = element_blank(),
+        axis.ticks = element_blank(), axis.title = element_text(size = 11),
+        legend.title = element_text(size = 11))
 
 discrete_plot <- last_plot()
 
@@ -41,39 +42,40 @@ discrete_plot <- last_plot()
 
 # Continuous case ---------------------------------------------------------
 
-mu0 <- seq(-2, 2, length.out = 50)
+mu0 <- seq(-2, 2, length.out = 100)
 
 df_conts <- lapply(mu0, function(m0) {
   data_frame(
     pst = pst,
-    y = sigmoid(rnorm(length(pst), pst, 0.01), m0, 10, 0.7),
+    y = sigmoid(pst, m0, 10, 0.7),
     mu0 = m0
   )
 })
 
 dfc <- bind_rows(df_conts)
 
-dfc_noise <- mutate(dfc, y = rnorm(length(y), y, .6))
+# dfc_noise <- mutate(dfc, y = rnorm(length(y), y, .6))
 
 dfc %>% arrange(mu0, pst) %>% 
   ggplot(aes(x = pst, y = y, group = mu0, color = mu0)) +
-  geom_point(data = dfc_noise) +
-  # geom_line(size = 1.5) + 
+  # geom_point(data = dfc_noise) +
+  geom_line(size = 1.5) + 
   scale_color_viridis(name = "Continuous\nCovariate") +
-  theme(legend.text = element_blank(), legend.direction = "horizontal",
-        legend.title = element_text(hjust = 0.5),
-        legend.position = "top", axis.text = element_blank(),
-        axis.ticks = element_blank(), axis.title = element_text(size = 11)) +
-  xlab("Patient trajectory") + ylab("Dynamic observable")
+  theme(legend.text = element_blank(), # legend.direction = "horizontal",
+        legend.title = element_text(size = 11),
+        legend.position = "right", axis.text = element_blank(),
+        axis.ticks = element_blank(), axis.title = element_text(size = 11),
+        legend.key.size = unit(.2, "cm")) +
+  xlab("Trajectory") + ylab("Gene expression")
 
 continuous_plot = last_plot()
 
-plot_grid(discrete_plot, continuous_plot)
+plot_grid(discrete_plot, continuous_plot, rel_widths = c(10, 11))
 
 phenotime_diagram <- last_plot()
 
-save(phenotime_diagram, file = "../figs/phenotime_diagram.Rdata")
-ggsave("../figs/phenotime_diagram.png", width = 9, height = 3)
+# save(phenotime_diagram, file = "../figs/phenotime_diagram.Rdata")
+ggsave("../figs/method_diagram.png", width = 9, height = 1.5)
 
 
 
